164e4fd9a2ecee315f18966dcfb1684554372284
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-03-19 17:41:44 -0400

    first commit

--- cal/CalTest.java
+++ cal/CalTest.java
@@ -4,12 +4,12 @@ public class CalTest {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		
-		cal.setOperandOne(10.5);
-		cal.setOperandTwo(5.2);
-		cal.setOperation("+");
-		cal.performOperation();
+		cal.performOperation(10.5);
+		cal.performOperation("+");
+		cal.performOperation(50.0);
+		cal.performOperation("/");
+		cal.performOperation(10);
+		cal.performOperation("=");
 		System.out.println(cal.getResult());
-		
-	
 	}
 }
